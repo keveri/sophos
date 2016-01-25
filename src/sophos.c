b232e419510a8622ab3c903192fb77f4d3f06beb
Matias Keveri <keveri@users.noreply.github.com>
2016-01-25 10:29:21 +0200

    Log socket bind/listen errors. Add const keywords and improve readability.

--- src/sophos.c
+++ src/sophos.c
@@ -8,45 +8,55 @@
 #include "options.c"
 #include "wisdoms.c"
 
-struct sockaddr_in create_server(uint32_t port) {
+static void log_and_exit(const char *error) {
+    perror(error);
+    exit(1);
+}
+
+static void bind_and_listen_to_port(const uint32_t sock,
+                                    const struct sockaddr_in server) {
+    if (bind(sock, (struct sockaddr *)&server, sizeof(struct sockaddr)) < 0) {
+        log_and_exit("Failed to bind the server socket");
+    }
+    if (listen(sock, 1) < 0) {
+        log_and_exit("Failed to listen on the server socket");
+    }
+}
+
+static struct sockaddr_in create_server(const uint32_t port) {
     struct sockaddr_in server = {0};
-    server.sin_family      = AF_INET;
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port        = htons(port);
+    server.sin_family         = AF_INET;
+    server.sin_addr.s_addr    = htonl(INADDR_ANY);
+    server.sin_port           = htons(port);
     return server;
 }
 
-void process_connections(uint32_t sock, socklen_t socksize, warray wisdoms) {
+static void process_connections(const uint32_t sock, socklen_t socksize,
+                                const warray wisdoms) {
     struct sockaddr_in client;
     int32_t conn_sock = accept(sock, (struct sockaddr *)&client, &socksize);
     char *msg;
 
-    while(conn_sock)
-    {
+    while(conn_sock) {
         msg = pick_a_wisdom(wisdoms);
         printf("Connection from: %s\n", inet_ntoa(client.sin_addr));
-        send(conn_sock, msg, strlen(msg), 0); 
+        send(conn_sock, msg, strlen(msg), 0);
         close(conn_sock);
         conn_sock = accept(sock, (struct sockaddr *)&client, &socksize);
     }
-
 }
 
 int main(int argc, char **argv) {
-    options opt = parse_options(argc, argv);
     srand(time(NULL));
-    struct sockaddr_in server = create_server(opt.port);
-    const socklen_t socksize = sizeof(struct sockaddr_in);
-    const uint32_t sock = socket(AF_INET, SOCK_STREAM, 0);
-    warray wisdoms = read_wisdoms(opt.fname);
+    const options opt               = parse_options(argc, argv);
+    const struct sockaddr_in server = create_server(opt.port);
+    const socklen_t socksize        = sizeof(struct sockaddr_in);
+    const uint32_t sock             = socket(AF_INET, SOCK_STREAM, 0);
+    const warray wisdoms            = read_wisdoms(opt.fname);
 
-    /* bind to port and start listening */
-    bind(sock, (struct sockaddr *)&server, sizeof(struct sockaddr));
-    listen(sock, 1);
-
+    bind_and_listen_to_port(sock, server);
     process_connections(sock, socksize, wisdoms);
 
-    /* clean up */
     clean_wisdoms(wisdoms);
     close(sock);
     return 0;
